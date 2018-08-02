d36dc26c25e4945243809945e25ee4f8fb528a46
1409340616@qq.com <15808034733@163.com>
2018-08-02 15:14:45 +0800

    day1

---
+++ concurrencyday1/src/main/java/com/example/concurrencyday1/Concurrencyday1Application.java
@@ -0,0 +1,12 @@
+package com.example.concurrencyday1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Concurrencyday1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Concurrencyday1Application.class, args);
+	}
+}
